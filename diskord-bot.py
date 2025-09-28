9112a232154f2996d82817862e1e32f53e177c89
siemacotam2225-ui <siema.cotam2225@gmail.com>
2025-09-28 16:48:14 +0200

    Add files via upload

--- diskord-bot.py
+++ diskord-bot.py
@@ -1,10 +1,11 @@
+import os
 import discord
 import requests
 from bs4 import BeautifulSoup
 import asyncio
 
 # --- Konfiguracja ---
-DISCORD_TOKEN = ''  # Wklej tutaj token swojego bota
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')  # Wklej tutaj token swojego bota
 CHANNEL_ID = '1421853540665786530'  # Wklej ID kanału, na który mają być wysyłane powiadomienia
 OLX_URL = 'https://www.olx.pl/elektronika/telefony/q-iphone/?search%5Bfilter_float_price:to%5D=2000&search%5Bfilter_enum_state%5D%5B0%5D=used&search%5Bfilter_enum_state%5D%5B1%5D=new&search%5Bfilter_enum_phonemodel%5D%5B0%5D=iphone-air&search%5Bfilter_enum_phonemodel%5D%5B1%5D=iphone-13-pro-max&search%5Bfilter_enum_phonemodel%5D%5B2%5D=iphone-17&search%5Bfilter_enum_phonemodel%5D%5B3%5D=iphone-12&search%5Bfilter_enum_phonemodel%5D%5B4%5D=iphone-12-mini&search%5Bfilter_enum_phonemodel%5D%5B5%5D=iphone-12-pro&search%5Bfilter_enum_phonemodel%5D%5B6%5D=iphone-12-pro-max&search%5Bfilter_enum_phonemodel%5D%5B7%5D=iphone-11-pro&search%5Bfilter_enum_phonemodel%5D%5B8%5D=iphone-11-pro-max&search%5Bfilter_enum_phonemodel%5D%5B9%5D=iphone-17-pro&search%5Bfilter_enum_phonemodel%5D%5B10%5D=iphone-17-pro-max&search%5Bfilter_enum_phonemodel%5D%5B11%5D=iphone-15-pro&search%5Bfilter_enum_phonemodel%5D%5B12%5D=iphone-16&search%5Bfilter_enum_phonemodel%5D%5B13%5D=iphone-16-plus&search%5Bfilter_enum_phonemodel%5D%5B14%5D=iphone-16pro&search%5Bfilter_enum_phonemodel%5D%5B15%5D=iphone-16-pro-max&search%5Bfilter_enum_phonemodel%5D%5B16%5D=iphone-15&search%5Bfilter_enum_phonemodel%5D%5B17%5D=iphone-15-plus&search%5Bfilter_enum_phonemodel%5D%5B18%5D=iphone-15-pro-max&search%5Bfilter_enum_phonemodel%5D%5B19%5D=iphone-14&search%5Bfilter_enum_phonemodel%5D%5B20%5D=iphone-14-plus&search%5Bfilter_enum_phonemodel%5D%5B21%5D=iphone-14-pro&search%5Bfilter_enum_phonemodel%5D%5B22%5D=iphone-14-pro-max&search%5Bfilter_enum_phonemodel%5D%5B23%5D=iphone-13&search%5Bfilter_enum_phonemodel%5D%5B24%5D=iphone-13-mini&search%5Bfilter_enum_phonemodel%5D%5B25%5D=iphone-13-pro'# Przykładowy link do wyszukiwania
 CHECK_INTERVAL = 1  # Czas w sekundach, co ile bot ma sprawdzać nowe ogłoszenia (np. 300 = 5 minut)
